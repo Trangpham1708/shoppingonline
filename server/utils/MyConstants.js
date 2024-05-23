a431b964374b95e1e0a6edf790efd56a8f1b997b
Trangpham1708 <104095531+Trangpham1708@users.noreply.github.com>
2024-05-24 02:31:47 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.zyo4nfs.mongodb.net',
+    DB_USER: 'trangpham1708',
+    DB_PASS: 'Trangpham47',
+    DB_DATABASE: 'Chanchan',
+    JWT_SECRET: 'caiqqgicxdc',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'trangphamm1708@gmail.com', // gmail service
+    EMAIL_PASS: 'njsjyrljddgjkacu'
+  };
+  module.exports = MyConstants;
